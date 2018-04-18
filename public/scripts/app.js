6ef8a83e144beca5ffe406cf55a6e4ad3d644c7b
Alex Afeltra <afeltra.alex@gmail.com>
2018-04-17 23:34:53 -0400

    conditional rendering in jsx

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,10 +3,74 @@
 console.log('app.js is running');
 
 // JSX - Javascript XML
+
+var app = {
+	title: 'React Todo App',
+	subtitle: 'This is a todo app',
+	options: ['One', 'Two']
+};
 var template = React.createElement(
-  'p',
-  null,
-  'hello'
+	'div',
+	null,
+	React.createElement(
+		'h1',
+		null,
+		app.title
+	),
+	app.subtitle && React.createElement(
+		'p',
+		null,
+		app.subtitle
+	),
+	app.options.length > 0 ? "heres your options" : "no options",
+	React.createElement(
+		'ol',
+		null,
+		React.createElement(
+			'li',
+			null,
+			'list one'
+		),
+		React.createElement(
+			'li',
+			null,
+			'list two'
+		)
+	)
+);
+
+var user = {
+	name: 'Alex',
+	age: 26,
+	location: 'Nepean'
+};
+
+function getLocation(location) {
+	if (location) {
+		return React.createElement(
+			'p',
+			null,
+			'Location: ',
+			location
+		);
+	}
+}
+
+var templateTwo = React.createElement(
+	'div',
+	null,
+	React.createElement(
+		'h1',
+		null,
+		user.name ? user.name : 'Anonymous'
+	),
+	user.age && user.age >= 18 && React.createElement(
+		'p',
+		null,
+		'Age: ',
+		user.age
+	),
+	getLocation(user.location)
 );
 
 var appRoot = document.getElementById('app');
--- src/app.js
+++ src/app.js
@@ -1,7 +1,44 @@
 console.log('app.js is running');
 
 // JSX - Javascript XML
-var template = <p>hello</p>;
+
+var app = {
+	title: 'React Todo App',
+	subtitle: 'This is a todo app',
+	options: ['One', 'Two']
+};
+var template = (
+	<div>
+		<h1>{app.title}</h1>
+		{app.subtitle && <p>{app.subtitle}</p>}
+		{/* {(app.options && app.options.length > 0) && <p>here are are your options {app.options}</p>} */}
+		{app.options.length > 0 ? "heres your options" : "no options"}
+		<ol>
+			<li>list one</li>
+			<li>list two</li>
+		</ol>
+	</div>
+);
+
+var user = {
+	name: 'Alex',
+	age: 26,
+	location: 'Nepean'
+};
+
+function getLocation(location) {
+	if (location) {
+		return <p>Location: {location}</p>;
+	}
+}
+
+var templateTwo = (
+	<div>
+		<h1>{user.name ? user.name : 'Anonymous'}</h1>
+		{(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+		{getLocation(user.location)}
+	</div>
+);
 
 var appRoot = document.getElementById('app');
 
