4a2237f1527e3881dd423ac8ba974843910c0537
Alex Afeltra <afeltra.alex@gmail.com>
2018-06-08 20:08:59 -0400

    events and attributes overview

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,43 +1,81 @@
 'use strict';
 
-//arguements object no longer bound with arrow functions
-var add = function add(a, b) {
-	//console.log(arguements);
-	return a + b;
+console.log('app.js is running');
+
+// JSX - Javascript XML
+
+var app = {
+	title: 'React Todo App',
+	subtitle: 'This is a todo app',
+	options: ['One', 'Two']
 };
 
-console.log(add(55, 1));
+var template = React.createElement(
+	'div',
+	null,
+	React.createElement(
+		'h1',
+		null,
+		app.title
+	),
+	app.subtitle && React.createElement(
+		'p',
+		null,
+		app.subtitle
+	),
+	app.options.length > 0 ? "heres your options" : "no options",
+	React.createElement(
+		'ol',
+		null,
+		React.createElement(
+			'li',
+			null,
+			'list one'
+		),
+		React.createElement(
+			'li',
+			null,
+			'list two'
+		)
+	)
+);
 
-//this keyword no longer bound 
-
-var user = {
-	name: 'Alex',
-	cities: ['Ottawa', 'Toronto', 'Montreal'],
-	printPlacesLived: function printPlacesLived() {
-		var _this = this;
-
-		return this.cities.map(function (city) {
-			return _this.name + ' has lived in ' + city;
-		});
-	}
+var count = 0;
+var addOne = function addOne() {
+	console.log('addOne');
 };
-
-console.log(user.printPlacesLived());
-
-//challenge
-var multiplier = {
-	//numbers - array
-	numbers: [1, 2, 3, 4],
-	//multiplyBy - integer
-	multiplyBy: 2,
-	//multiply - return new array where the numbers have been multiplied
-	multiply: function multiply() {
-		var _this2 = this;
-
-		return this.numbers.map(function (number) {
-			return number * _this2.multiplyBy;
-		});
-	}
+var minusOne = function minusOne() {
+	console.log('minusOne');
 };
+var reset = function reset() {
+	console.log('reset');
+};
+var templateTwo = React.createElement(
+	'div',
+	null,
+	React.createElement(
+		'h1',
+		null,
+		'Count: ',
+		count
+	),
+	React.createElement(
+		'button',
+		{ onClick: addOne },
+		'+1'
+	),
+	React.createElement(
+		'button',
+		{ onClick: minusOne },
+		'-1'
+	),
+	React.createElement(
+		'button',
+		{ onClick: reset },
+		'reset'
+	)
+);
 
-console.log(multiplier.multiply());
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(templateTwo, appRoot);
