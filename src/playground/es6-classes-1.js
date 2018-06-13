7ed927a64ef0f973b38af7da6f94a968e3a641bd
Alex Afeltra <afeltra.alex@gmail.com>
2018-06-13 13:40:23 -0400

    es6 classes intro part 2 subclasses

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -3,10 +3,51 @@ class Person {
 		this.name = name;
 		this.age = age;
 	}
+	getGreeting(){
+		return `Hi. I am ${this.name}.`;
+	}
 	getDescription() {
 		return `${this.name} is ${this.age} years old`;
 	}
 }
 
-const me = new Person('Alex', 27);
-console.log(me.getDescription());
+class Student extends Person {
+	constructor(name, age, major) {
+		super(name, age);
+		this.major = major;
+	}
+	hasMajor() {
+		return !!this.major;
+	}
+	getDescription(){
+		let description = super.getDescription();
+
+		if(this.hasMajor()) {
+			description += ` Their Major is ${this.major}.`;
+		}
+		return description;
+	}
+}
+
+class Traveler extends Person {
+	constructor(name, age, homeLocation) {
+		super(name, age);
+		this.homeLocation = homeLocation;
+	}
+	hasLocation() {
+		return !!this.homeLocation;
+	}
+	getGreeting() {
+		let greeting = super.getGreeting();
+		if(this.hasLocation()) {
+			greeting += ` I am visiting from ${this.homeLocation}.`;
+		}
+		return greeting;
+	}
+}
+
+const me = new Traveler('Alex', 27, 'Ottawa');
+console.log(me.getGreeting());
+
+const other = new Traveler();
+console.log(other.getGreeting());
